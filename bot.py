af6690edd99e639009fc32c463ae8bfec3fa5afe
Samuel Echevarria <58719082+sechevarria98@users.noreply.github.com>
2021-01-12 22:08:11 -0600

    minor change

--- bot.py
+++ bot.py
@@ -22,4 +22,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('YOURE TOKEN HERE')
+client.run('YOUR TOKEN HERE')
